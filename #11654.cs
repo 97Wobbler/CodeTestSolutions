726eb8ee6eda705f98e1105f0b4c7240d3d60326
97wobbler <cwobbler@naver.com>
2023-11-06 11:22:37 +0900

    Initial commit

---
+++ #11654.cs
@@ -0,0 +1,10 @@
+class Choi
+{
+    static void Main()
+    {
+        char letter = char.Parse(Console.ReadLine());
+        int result = (int)letter;
+
+        Console.WriteLine(result);
+    }
+}
